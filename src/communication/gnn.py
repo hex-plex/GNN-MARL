4083591f10e4e41bde88fc856cc4cec51b18ed4a
hex-plex <sendhilsom7@gmail.com>
2021-12-30 22:32:45 +0530

    completed implementation of GNN-GAT

--- src/communication/gnn.py
+++ src/communication/gnn.py
@@ -41,9 +41,12 @@ class GATComm(torch.nn.Module):
         self.convs.append(GCNConv(self.args.msg_hidden_dim, self.args.msg_out_size))    
     
     def forward(self,x, adj_matrix):
-        for i in range(self.args.num_layers):
-            x = self.convs[i](x, edge_index)
-            if (i+1)<self.args.num_layers:
-                x = F.elu(x)
-                x = F.dropout(x, p=0.2, training=self.training) 
-        return x 
+        x_out = []
+        for x_in, am_in in zip(torch.unbind(x, dim=0), torch.unbind(adj_matrix, dim=0)):
+            for i in range(self.args.num_layers):
+                x_in = self.convs[i](x_in, dense_to_sparse(am_in)[0])
+                if (i+1)<self.args.num_layers:
+                    x_in = F.elu(x_in)
+                    x_in = F.dropout(x_in, p=0.2, training=self.training)
+            x_out.append(x_in) 
+        return torch.stack(x_out, dim=0)
